d60b5c51db1e556b974e29fdbfc75dd8fe926317
June Kim <97197559+juneyk1@users.noreply.github.com>
2025-04-18 14:09:49 -0700

    moving backend to server, dotenv setup

--- server/backend/models.py
+++ server/backend/models.py
@@ -5,6 +5,13 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+# POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+# POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+# POSTGRES_DB = os.getenv("POSTGRES_DB")
+# POSTGRES_USER = os.getenv("POSTGRES_USER")
+# POSTGRES_PASS = os.getenv("POSTGRES_PASS")
+
 # temp
 POSTGRES_URL = "postgresql+psycopg2://postgres:password@localhost:5432/grc"
 print("Using DB URL:", POSTGRES_URL)
